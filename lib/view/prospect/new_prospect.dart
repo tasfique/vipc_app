1bdaabbb66befc088304b17073690506cc4cfbd5
kulakhmet25@gmail.com <kulakhmet25@gmail.com>
2021-02-18 21:46:38 +0800

    1. New Prospect page done

--- lib/view/prospect/new_prospect.dart
+++ lib/view/prospect/new_prospect.dart
@@ -28,71 +28,41 @@ class _AddProspectState extends State<AddProspect> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(15),
-      child: Column(
-        children: [
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.all(10),
-            child: Text(
-              "Add new prospect",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-          ),
-          SizedBox(height: 20),
-          _buildNameTextField(),
-          SizedBox(height: 15),
-          _contactNoTextField(),
-          SizedBox(height: 15),
-          _buildDropdownList(),
-          SizedBox(height: 15),
-          Container(
-            margin: EdgeInsets.all(5),
-            width: MediaQuery.of(context).size.width,
-            height: 150,
-            child: TextField(
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Memo',
-              ),
-            ),
-          ),
-          SizedBox(height: 10),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  margin: EdgeInsets.all(15),
-                  padding: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    color: Colors.black12,
-                  ),
-                  child: Text("Save"),
+      height: double.infinity,
+      child: SingleChildScrollView(
+        physics: AlwaysScrollableScrollPhysics(),
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Container(
+              alignment: Alignment.centerLeft,
+              padding: EdgeInsets.all(10),
+              child: Text(
+                "Add new prospect",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
                 ),
               ),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => ProspectView()));
-                },
-                child: Container(
-                  margin: EdgeInsets.all(15),
-                  padding: EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                    color: Colors.black12,
-                  ),
-                  child: Text("Back"),
-                ),
-              ),
-            ],
-          ),
-        ],
+            ),
+            SizedBox(height: 20),
+            _buildNameTextField(),
+            SizedBox(height: 15),
+            _contactNoTextField(),
+            SizedBox(height: 15),
+            _buildDropdownList(),
+            SizedBox(height: 30),
+            _buildTextFormField(),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                _buildSaveBtn(),
+                _buildBackBtn(),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -221,10 +191,13 @@ class _AddProspectState extends State<AddProspect> {
           height: 60.0,
           child: DropdownButton(
             hint: Container(
-              child: Text("Select the type",
-                  style: TextStyle(color: Colors.white70)),
+              child: Text(
+                "Select the type",
+                style: TextStyle(color: Colors.white70),
+              ),
             ),
             isExpanded: true,
+            iconEnabledColor: Colors.white,
             value: selectedType,
             onChanged: (String value) {
               setState(() {
@@ -249,6 +222,118 @@ class _AddProspectState extends State<AddProspect> {
           ),
         ),
       ],
+    );
+  }
+
+  Widget _buildTextFormField() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+            color: Colors.white24,
+            borderRadius: BorderRadius.circular(10.0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 6.0,
+                offset: Offset(0, 2),
+              ),
+            ],
+          ),
+          child: TextFormField(
+            // controller: _usernameController,
+            minLines: 6,
+            keyboardType: TextInputType.text,
+            style: TextStyle(
+              color: Colors.white,
+            ),
+            decoration: InputDecoration(
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.fromLTRB(15, 7, 0, 7),
+              hintText: 'Memo . . .',
+              hintStyle: TextStyle(
+                color: Colors.white70,
+              ),
+            ),
+            maxLines: null,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildSaveBtn() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (_) => new AlertDialog(
+              title: new Text("VIPC Message"),
+              content: new Text("Successfully saved!"),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Close'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) {
+                      return ProspectView();
+                    }));
+                  },
+                )
+              ],
+            ),
+          );
+        },
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        color: Colors.amber[400],
+        child: Text(
+          'Save',
+          style: TextStyle(
+            color: Colors.black,
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildBackBtn() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      child: RaisedButton(
+        elevation: 5.0,
+        onPressed: () {
+          Navigator.of(context).pop();
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return ProspectView();
+          }));
+        },
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        color: Colors.amber[400],
+        child: Text(
+          'Back',
+          style: TextStyle(
+            color: Colors.black,
+            letterSpacing: 1.5,
+            fontSize: 18.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
     );
   }
 }
